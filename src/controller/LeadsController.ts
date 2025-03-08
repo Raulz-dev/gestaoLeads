3fe867ae08088ec26231f9ba380a27acaeab1cff
Raul Pontes <raulluar11222@gmail.com>
2025-03-08 12:08:20 -0300

    FEAT- Paginação e filtragem de leads

--- src/controller/LeadsController.ts
+++ src/controller/LeadsController.ts
@@ -1,13 +1,40 @@
 import { Handler } from "express";
 import { prisma } from "../database";
-import { CreateLeadRequestSchema } from "./schema/LeadsRequestSchema";
+import { CreateLeadRequestSchema, getLeadsRequestSchema, UpdateLeadRequestSchema } from "./schema/LeadsRequestSchema";
 import { HttpError } from "../Error/httpError";
+import { Prisma } from "@prisma/client";
 
 export class leadController {
   index: Handler = async (req, res, next) => {
     try {
-      const leads = await prisma.lead.findMany();
-      res.status(200).json(leads);
+      const query = getLeadsRequestSchema.parse(req.query);
+      const { page = "1", pageSize = "10", name, status, sortBy = "name", order = "asc", email } = query;
+      const pageNumber = +page;
+      const pageSizeNumber = +pageSize;
+
+      const where: Prisma.LeadWhereInput = {};
+
+      if (name) where.name = { contains: name, mode: "insensitive" };
+      if (email) where.email = { contains: email, mode: "insensitive" };
+      if (status) where.status = status;
+
+      const leads = await prisma.lead.findMany({
+        where,
+        skip: (pageNumber - 1) * pageSizeNumber,
+        take: pageSizeNumber,
+        orderBy: { [sortBy]: order },
+      });
+      const total = await prisma.lead.count({ where });
+
+      res.status(200).json({
+        data: leads,
+        meta: {
+          page: pageNumber,
+          pageSize: pageSizeNumber,
+          total,
+          totalPages: Math.ceil(total / pageSizeNumber),
+        },
+      });
     } catch (error) {
       next(error);
     }
@@ -42,27 +69,32 @@ export class leadController {
   };
   update: Handler = async (req, res, next) => {
     try {
-      const { name, email, phone, status } = req.body;
-      const newLead = await prisma.lead.update({
-        where: {
-          id: +req.params.id,
-        },
-        data: {
-          name,
-          email,
-          phone,
-          status,
-        },
-      });
-      res.json(newLead);
+      const id = Number(req.params.id);
+      const body = UpdateLeadRequestSchema.parse(req.body);
+
+      const leadExists = await prisma.lead.findUnique({ where: { id } });
+      if (!leadExists) throw new HttpError(404, "lead não encontrado");
+
+      const updatedLead = await prisma.lead.update({ data: body, where: { id } });
+
+      res.json(updatedLead);
     } catch (error) {
       next(error);
     }
   };
+
   delete: Handler = async (req, res, next) => {
-    const deletedLead = await prisma.lead.delete({
-      where: { id: +req.params.id },
-    });
-    res.json(deletedLead);
+    try {
+      const id = Number(req.params.id);
+
+      const leadExists = await prisma.lead.findUnique({ where: { id } });
+      if (!leadExists) throw new HttpError(404, "lead não encontrado");
+
+      const deletedLead = await prisma.lead.delete({ where: { id } });
+
+      res.json({ deletedLead });
+    } catch (error) {
+      next(error);
+    }
   };
 }
--- src/controller/schema/LeadsRequestSchema.ts
+++ src/controller/schema/LeadsRequestSchema.ts
@@ -1,8 +1,27 @@
 import { z } from "zod";
 
+const leadStatusSchema = z.enum(["New", "Contacted", "Qualified", "Converted", "Unresponsive", "Disqualified", "Archived"]);
+
+export const getLeadsRequestSchema = z.object({
+  page: z.string().optional(),
+  pageSize: z.string().optional(),
+  name: z.string().optional(),
+  status: leadStatusSchema.optional(),
+  email: z.string().optional(),
+  sortBy: z.enum(["name", "status", "createdAt"]).optional(),
+  order: z.enum(["asc", "desc"]).optional(),
+});
+
 export const CreateLeadRequestSchema = z.object({
   name: z.string(),
   email: z.string(),
   phone: z.string(),
-  status: z.enum(["New", "Contacted", "Qualified", "Converted", "Unresponsive", "Disqualified", "Archived"]).optional(),
+  status: leadStatusSchema.optional(),
+});
+
+export const UpdateLeadRequestSchema = z.object({
+  name: z.string().optional(),
+  email: z.string().optional(),
+  phone: z.string().optional(),
+  status: leadStatusSchema.optional(),
 });
